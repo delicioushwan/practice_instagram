718bd7e15383bf09c415ad8223242e5f7fee101c
delicioushwan <tmdghks0217@gmail.com>
2019-06-25 19:12:47 +0900

    signup validation in front

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     "jsx-a11y/click-events-have-key-events": 0,
     "jsx-a11y/no-static-element-interactions": 0,
     "react/destructuring-assignment": 0,
-    "react/prop-types": 0,    
+    "react/prop-types": 0,
+    "no-nested-ternary": 0,
   },
 };
--- client/src/component/signup.js
+++ client/src/component/signup.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import Axios from 'axios';
 import { hot } from 'react-hot-loader';
-import '../style/signin.css';
+import { regex } from '../inputtest';
+import '../style/signup.css';
 
 export default hot(module)(class extends React.Component {
   constructor() {
     super();
     this.state = {
-      idInput: '',
-      passwordInput: '',
-      checkpassword: '',
-      emailInput: '',
-      nameInput: '',
     };
   }
 
   render() {
     const {
       idInput, passwordInput, checkpassword, emailInput, nameInput,
+    } = this.props.home.state;
+    const {
+      userId, password, checkPw, email, userName,
     } = this.state;
 
+    const updateHome = state => this.props.home.setState(state);
+
     const inputHandler = (e, input) => {
-      this.setState({ [input]: e.target.value });
+      updateHome({ [input]: e.target.value });
     };
 
     const submit = (e) => {
       e.preventDefault();
+      console.log('runnnnnn');
       const body = {};
       Array.from(this.form.getElementsByTagName('INPUT')).forEach((input) => { body[input.name] = input.value; });
       Axios.request({
@@ -36,44 +38,75 @@ export default hot(module)(class extends React.Component {
         .then(res => console.log(res))
         .catch(err => console.log(err));
       this.props.close();
-      this.setState({
-        idInput: '',
-        passwordInput: '',
-        checkpassword: '',
-        emailInput: '',
-        nameInput: '',
-      });
+    };
+    const vaildationCheck = (e) => {
+      const input = e.target.value;
+      const target = e.target.name;
+      switch (target) {
+        case 'userId':
+          if (!regex.name.test(input)) return this.setState({ userId: '아이디는 2자이상 15자이하여야 합니다.' });
+          if (!regex.no_korean.test(input)) return this.setState({ userId: '아이디에는 한글을 포함할 수 없습니다.' });
+          return this.setState({ userId: '' });
+        case 'password':
+          if (!regex.name.test(input)) return this.setState({ password: '비밀번호는 2자이상 15자이하여야 합니다.' });
+          return this.setState({ password: '' });
+        case 'checkPw':
+          if (passwordInput !== input) return this.setState({ checkPw: '동일한 비밀번호를 입력해야 합니다.' });
+          return this.setState({ checkPw: '' });
+        case 'email':
+          if (!regex.email.test(input)) return this.setState({ email: '올바른형식의 이메일을 입력해야 합니다.' });
+          return this.setState({ email: '' });
+        case 'userName':
+          if (!input) return this.setState({ userName: '이용자 이름을 입력해야 합니다.' });
+          return this.setState({ userName: '' });
+        default:
+          console.log(target);
+      }
+    };
+
+    const submitActive = (e) => {
+      if (e) { e.preventDefault(); }
+      return userId === '' && password === '' && checkPw === '' && email === '' && userName === '';
+    };
+
+    const inputFn = (e, input) => {
+      inputHandler(e, input);
+      vaildationCheck(e);
     };
 
     return (
-      <div className="sign_in">
+      <div className="sign_up">
         <h2>
           친구들의 사진과 동영상을 보려면 가입하세요.
         </h2>
-        <form ref={(node) => { this.form = node; }}>
+        <form ref={(node) => { this.form = node; }} method="post">
           <div>
-            <input
-              type="text"
-              name="userId"
-              onChange={e => inputHandler(e, 'idInput')}
-              value={idInput}
-              placeholder=" 사용자 아이디"
-            />
+            <input type="text" name="userId" onChange={e => inputFn(e, 'idInput')} value={idInput} placeholder=" 사용자 아이디" />
           </div>
+          <span className={this.state.userId ? 'error' : 'noshow'}>{this.state.userId}</span>
+
           <div>
-            <input type="password" name="password" onChange={e => inputHandler(e, 'passwordInput')} value={passwordInput} placeholder=" 비밀번호" />
+            <input type="password" name="password" onChange={e => inputFn(e, 'passwordInput')} value={passwordInput} placeholder=" 비밀번호" />
           </div>
+          <span className={this.state.password ? 'error' : 'noshow'}>{this.state.password}</span>
+
           <div>
-            <input type="password" onChange={e => inputHandler(e, 'checkpassword')} value={checkpassword} placeholder=" 비밀번호 확인" />
+            <input type="password" name="checkPw" onChange={e => inputFn(e, 'checkpassword')} value={checkpassword} placeholder=" 비밀번호 확인" />
           </div>
+          <span className={this.state.checkPw ? 'error' : 'noshow'}>{this.state.checkPw}</span>
+
           <div>
-            <input type="text" name="email" onChange={e => inputHandler(e, 'emailInput')} value={emailInput} placeholder="  이메일 주소" />
+            <input type="text" name="email" onChange={e => inputFn(e, 'emailInput')} value={emailInput} placeholder="  이메일 주소" />
           </div>
+          <span className={this.state.email ? 'error' : 'noshow'}>{this.state.email}</span>
+
           <div>
-            <input type="text" name="userName" onChange={e => inputHandler(e, 'nameInput')} value={nameInput} placeholder=" 사용자 이름" />
+            <input type="text" name="userName" onChange={e => inputFn(e, 'nameInput')} value={nameInput} placeholder=" 사용자 이름" />
           </div>
+          <span className={this.state.userName ? 'error' : 'noshow'}>{this.state.userName}</span>
+
           <div>
-            <button type="submit" onClick={submit} className={idInput && passwordInput && checkpassword && emailInput && nameInput ? 'active' : ''}>
+            <button type="submit" onClick={e => submitActive(e) && submit(e)} className={submitActive() ? 'active' : ''}>
               가입
             </button>
           </div>
