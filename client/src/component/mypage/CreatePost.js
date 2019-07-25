c6ac23777d4569cb5e60d008e338469b228ba2d7
delicioushwan <tmdghks0217@gmail.com>
2019-07-25 17:53:39 +0900

    kk

--- client/src/component/mypage/CreatePost.js
+++ client/src/component/mypage/CreatePost.js
@@ -53,17 +53,22 @@ export default hot(module)(class extends Component {
       <form onSubmit={this.state.imgsUrl.length !== 0 && this.handleSubmit}>
         <input name="uploadImage" type="file" multiple onChange={this.pickPictures} value={this.state.Picture} />
         <div>
-          {this.state.imgsUrl && this.state.imgsUrl.map((url, i) => (
-            <div key={i}>
-              <img src={url} alt="" />
-            </div>
-          ))}
+          {this.state.imgsUrl && this.state.imgsUrl.map((url, i) => {
+            if (i < 4) {
+              return (
+                <div key={i}>
+                  <img src={url} alt="" />
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
         <div>
           <input placeholder="문구 입력" onChange={(e) => { this.setState({ content: e.target.value }); }} />
         </div>
         <div style={{ margin: '0' }}>
-          <div className={this.state.imgsUrl.length !== 0 ? '' : 'create_submit_err'}>사진은 필수입니다!!</div>
+          <div className={this.state.imgsUrl.length !== 0 ? '' : 'create_submit_err'}>사진은 필수입니다!!(최대 4장!!!!!)</div>
           <input className="create_post_submit" type="submit" />
         </div>
       </form>
